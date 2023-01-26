4c6379db39c51df7ce05f132687e6e304df6343c
haeun <shy624@naver.com>
2023-01-26 16:37:06 +0900

    class func ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>Hello World!</h1>
+        <FuncComp initNumber={2}></FuncComp>
+        <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
+function FuncComp(props) {
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+
+  // var dateState = useState((new Date()).toString());
+  // var _date = dateState[0];
+  // var setDate = numberState[1];
+
+  var [_date, setDate] = useState((new Date()).toString());
+
+  console.log('numberState', numberState);
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+          <input type="button" value="random" onClick={
+            function(){
+              setDate((new Date()).toString());
+            }
+          }></input>
+    </div>
+  );
+}
+
+class ClassComp extends React.Component{
+  state = {
+    number : this.props.initNumber
+  }
+  render(){
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number:Math.random()});
+          }.bind(this)
+        }></input>
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date:(new Date().toString)});
+          }.bind(this)
+        }></input>
+      </div>
+    )
+  }
+}
 export default App;
