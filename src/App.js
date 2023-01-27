0beb0a9fb01dd03492c3b30004991ff9ffb5b856
haeun <shy624@naver.com>
2023-01-27 11:02:24 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  var [classShow, setClassShow] = useState(true);
   return (
     <div className="container">
         <h1>Hello World!</h1>
-        <FuncComp initNumber={2}></FuncComp>
-        <ClassComp initNumber={2}></ClassComp>
-    </div>
+        <FuncComp initNumber={123}></FuncComp>
+        {classShow ? 
+        <ClassComp initNumber={2}></ClassComp> : null }
+
+        <input type="button" value="remove Class"
+          onClick={
+            function(){
+              setClassShow(false)
+            }
+          }
+          ></input>
+        
+        </div>
   );
 }
+var funcStyle = 'color:white;background:tomato'; // class방식과 구분
+var funcId = 0;
 
 function FuncComp(props) {
   var numberState = useState(props.initNumber);
@@ -21,8 +34,12 @@ function FuncComp(props) {
   // var setDate = numberState[1];
 
   var [_date, setDate] = useState((new Date()).toString());
-
   console.log('numberState', numberState);
+
+  useEffect(function() {
+    console.log('%cfunc => useEffect 실행 '+(++funcId), funcStyle);
+  });
+
   return (
     <div className="container">
       <h2>function style component</h2>
@@ -30,18 +47,38 @@ function FuncComp(props) {
       <p>Date : {_date}</p>
           <input type="button" value="random" onClick={
             function(){
+              setNumber(Math.random());
+            }
+          }></input>
+          <input type="button" value="Date" onClick={
+            function(){
               setDate((new Date()).toString());
             }
           }></input>
     </div>
   );
 }
-
+var classStyle = 'color:red';
 class ClassComp extends React.Component{
   state = {
-    number : this.props.initNumber
+    number : this.props.initNumber,
+    date : (new Date()).toString()
+  }
+  componentWillMount(){
+    console.log('%cclass => componentWillMount', classStyle )
+  }
+  componentDidMount(){
+    console.log('%cclass => componentDidMount', classStyle )
+  }
+
+  // state 값 변경 시 render() 실행 여부 결정
+  // true : 실행, false : 실행 안 함
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('%cclass => this.shouldComponentUpdate 실행',classStyle);
+    return true;
   }
   render(){
+    console.log('%cclass => componentWillMount, componentWillUpdate', classStyle )
     return (
       <div className="container">
         <h2>class style component</h2>
@@ -55,7 +92,7 @@ class ClassComp extends React.Component{
         <input type="button" value="date" onClick={
           function(){
             this.setState({date:(new Date().toString)});
-          }.bind(this)
+          }
         }></input>
       </div>
     )
