9852f438e0aa5c749ae29c98fad2a1e99bd13dc2
haeun <shy624@naver.com>
2023-01-27 11:58:51 +0900

    add control component

---
+++ src/components/Control.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+class Control extends Component {
+    render(){
+        return(
+            <ul>
+                <li><a href="/create">create</a></li>
+                <li><a href="/update">create</a></li>
+                <li><input type="button" value="delete"></input></li>
+            </ul>
+        );
+    }
+}
+export default Control;
